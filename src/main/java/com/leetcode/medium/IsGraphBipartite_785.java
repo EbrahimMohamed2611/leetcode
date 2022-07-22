451c1411a5138e93550c8b32006cd187155b37cc
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2022-07-22 14:45:30 +0200

    785.Is Graph Bipartite? using java

--- src/main/java/com/leetcode/medium/IsGraphBipartite_785.java
+++ src/main/java/com/leetcode/medium/IsGraphBipartite_785.java
@@ -5,9 +5,10 @@ import java.util.*;
 // https://leetcode.com/problems/is-graph-bipartite/
 public class IsGraphBipartite_785 {
     // First using BFS
+
     /**
      * Runtime: 1 ms, faster than 88.90% of Java online submissions for Is Graph Bipartite?.
-     * Memory Usage: 53.6 MB, less than 63.60% of Java online submissions for Is Graph Bipartite?.
+     * Memory Usage: 42.8 MB, less than 94.54% of Java online submissions for Is Graph Bipartite?.
      */
     enum Color {READ, BLACK}
 
@@ -36,6 +37,37 @@ public class IsGraphBipartite_785 {
                     return false;
                 }
             }
+        }
+        return true;
+    }
+
+    // Using DFS
+
+    /**
+     * Runtime: 1 ms, faster than 88.90% of Java online submissions for Is Graph Bipartite?.
+     * Memory Usage: 53.6 MB, less than 63.60% of Java online submissions for Is Graph Bipartite?.
+     */
+    private static boolean isBipartite(int[][] adjacencyList, int v){
+        Color[] colors = new Color[v+1];
+        for (int i = 1; i <= v; i++) {
+            if(!isBipartite(adjacencyList,i,colors))
+                return false;
+        }
+        return true;
+    }
+
+
+    private static boolean isBipartite(int[][] adjacencyList, int node, Color[] colors) {
+        if (colors[node] == null)
+            colors[node] = Color.READ;
+
+        for (int adjacencyNode : adjacencyList[node]) {
+            if (colors[adjacencyNode] == null) { // That's mean it's not visiting
+                colors[adjacencyNode] = colors[node] == Color.READ ? Color.BLACK : Color.READ;
+                if (!isBipartite(adjacencyList, adjacencyNode, colors))
+                    return false;
+            } else if (colors[adjacencyNode] == colors[node])
+                return false;
         }
         return true;
     }
